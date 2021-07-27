0d0b23e10a1e617644f4256e0abddf3d62fb8943
Joy Valdez <43526824+jvaldez90@users.noreply.github.com>
2021-07-27 08:06:06 -0400

    Update books.py

--- books.py
+++ books.py
@@ -1,1 +1,74 @@
-books = {}
+# This File contains a dictionary of books 
+# that exsists in our library.
+books = {
+  1 : {
+    "title" : "The Girl With The Dragon Tattoo",
+    "author" : "Stieg Larsson",
+    "genre" : "Crime, Mystery, Thriller",
+    "year" : 2005,
+    "call_number" : "NL Lar2005.s1"
+  },
+  2 : {
+    "title" : "Evermore",
+    "author" : "Alyson Noël",
+    "genre" : "Fantasy",
+    "year" : 2009,
+    "call_number" : "NL Noë2009.s1"
+  },
+  3 : {
+    "title" : "Glass Houses",
+    "author" : "Rachel Caine",
+    "genre" : "Fantasy, Young Adult",
+    "year" : 2006,
+    "call_number" : "NL Cai2006.s1"
+  },
+  4 : {
+    "title" : "Immortal Lycanthropes",
+    "author" : "Hal Johnson",
+    "genre" : "Young Adult Fiction, Paranormal Fiction",
+    "year" : 2012,
+    "call_number" : "NL Joh2012"
+  },
+  5 : {
+    "title" : "Fool",
+    "author" : "Christopher Moore",
+    "genre" : "Comedy, Abursdist Fiction",
+    "year" : 2009,
+    "call_number" : "NL Moo2009.s1"
+  },
+  6 : {
+    "title" : "ABSOLUTE C++ Sixth Edition",
+    "author" : "Walter J. Savitch",
+    "genre" : "Nonfiction, Computer Science, Programming",
+    "year" : 2015,
+    "call_number" : "TB Sav2015.ed6"
+  },
+  7 : {
+    "title" : "Precalculus: Mathematics for Calculus Sixth Edition",
+    "author" : "James Stewart",
+    "genre" : "Nonfiction, Mathematics",
+    "year" : 2011,
+    "call_number" : "TB Stw2001.ed6"
+  },
+  8 : {
+    "title" : "Bleach Volume 1: The Death and the Strawberry",
+    "author" : "Tite Kubo",
+    "genre" : "Manga, Adventure, Supernatual",
+    "year" : 2001,
+    "call_number" : "MG Kub2001.v1"
+  },
+  9 : {
+    "title" : "Countdown to French: Learn to Communicate in 24 hours",
+    "author" : "Gail Stein",
+    "genre" : "Nonfiction, Education, Languages",
+    "year" : 2004,
+    "call_number" : "LA Sti2004.fr"
+  },
+  10 : {
+    "title" : "Criminology for Dummies",
+    "author" : "Steven Briggs",
+    "genre" : "Nonfiction, Education, Psychology, Criminology",
+    "year" : 2019,
+    "call_number" : "TB Bri2019"
+  },
+}
